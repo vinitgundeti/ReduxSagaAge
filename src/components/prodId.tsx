f44fa05a03c5fd8cd2feaf5b920b65a4af19d348
vinit gundeti <vgundeti123@gmail.com>
2023-10-04 11:14:02 +0530

    basic setup

--- src/components/prodId.tsx
+++ src/components/prodId.tsx
@@ -1,15 +1,27 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import {prevProductClick, nextProductClick} from '../redux/actions';
+import {
+  prevProductClick,
+  nextProductClick,
+  //  searchProduct
+} from '../redux/actions';
+import {useState} from 'react';
 
 const ProdId = () => {
   const id = useSelector((state: any) => state.id);
+  // const products = useSelector((state: any) => state.products);
+  const [textValue, setTextValue] = useState('');
   const dispatch = useDispatch();
   const increment = () => {
     dispatch(nextProductClick(1));
   };
   const decrement = () => {
     dispatch(prevProductClick(1));
+  };
+
+  const onInputChange = (e: string) => {
+    setTextValue(e);
+    // dispatch(searchProduct(e))
   };
   return (
     <View style={styles.centerAlign}>
@@ -18,6 +30,16 @@ const ProdId = () => {
         <Button title="Previous (-)" onPress={decrement} />
         <Button title="Next (+)" onPress={increment} />
       </View>
+
+      <TextInput
+        value={textValue}
+        style={styles.textInput}
+        onChangeText={onInputChange}
+      />
+
+      {/* {products?.map((item: any, index: number) => (
+        <Text key={index}>{item.title}</Text>
+      ))} */}
     </View>
   );
 };
@@ -38,6 +60,11 @@ const styles = StyleSheet.create({
   headerText: {
     fontWeight: '700',
     fontSize: 30,
+  },
+  textInput: {
+    borderWidth: 2,
+    width: '100%',
+    height: 40,
   },
 });
 
